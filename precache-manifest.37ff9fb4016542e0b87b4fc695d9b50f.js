a7af8c65db680689c0cc36bd0a852bf434363022
moviespack <vaperzin@gmail.com>
2021-07-07 21:52:43 +0530

    Updates

---
+++ precache-manifest.37ff9fb4016542e0b87b4fc695d9b50f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1663a343ee39f24e40de457b6ebc09",
+    "url": "/basket-app-react/index.html"
+  },
+  {
+    "revision": "93a05cd79ecbb202a27a",
+    "url": "/basket-app-react/static/css/2.2645ef61.chunk.css"
+  },
+  {
+    "revision": "435134f68208eb8db54b",
+    "url": "/basket-app-react/static/css/main.07216b04.chunk.css"
+  },
+  {
+    "revision": "93a05cd79ecbb202a27a",
+    "url": "/basket-app-react/static/js/2.383965ee.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/basket-app-react/static/js/2.383965ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435134f68208eb8db54b",
+    "url": "/basket-app-react/static/js/main.a184af51.chunk.js"
+  },
+  {
+    "revision": "722ad3b23e6d3ab7876f",
+    "url": "/basket-app-react/static/js/runtime-main.e661e68d.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/basket-app-react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/basket-app-react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/basket-app-react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/basket-app-react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/basket-app-react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
